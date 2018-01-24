c150b08d203d629aaedd85077c48a638189073ae
Jan Kašpar <jan.kaspar@cern.ch>
2018-01-24 17:27:27 +0100

    Added config parameters and their default values.

--- config.h
+++ config.h
@@ -23,7 +23,9 @@ struct Config
 	double cut4_a, cut4_c, cut4_si;
 
 	bool cut1_apply;
+	bool cut2_apply;
 	bool cut3_apply;
+	bool cut4_apply;
 
 	int LoadFrom(const string &f);
 };
@@ -37,6 +39,26 @@ int Config::LoadFrom(const string &f_in)
 	input_files = process.getParameter<vector<string>>("input_files");
 	
 	n_si = process.getParameter<double>("n_si");
+
+	cut1_apply = process.getParameter<bool>("cut1_apply");
+	cut1_a = process.getParameter<double>("cut1_a");
+	cut1_c = process.getParameter<double>("cut1_c");
+	cut1_si = process.getParameter<double>("cut1_si");
+
+	cut2_apply = process.getParameter<bool>("cut2_apply");
+	cut2_a = process.getParameter<double>("cut2_a");
+	cut2_c = process.getParameter<double>("cut2_c");
+	cut2_si = process.getParameter<double>("cut2_si");
+
+	cut3_apply = process.getParameter<bool>("cut3_apply");
+	cut3_a = process.getParameter<double>("cut3_a");
+	cut3_c = process.getParameter<double>("cut3_c");
+	cut3_si = process.getParameter<double>("cut3_si");
+
+	cut4_apply = process.getParameter<bool>("cut4_apply");
+	cut4_a = process.getParameter<double>("cut4_a");
+	cut4_c = process.getParameter<double>("cut4_c");
+	cut4_si = process.getParameter<double>("cut4_si");
 
 	return 0;
 }
--- config_base.py
+++ config_base.py
@@ -1,6 +1,25 @@
 import FWCore.ParameterSet.Config as cms
 
 config = cms.PSet(
-    n_si = cms.double(4.)
-)
+    n_si = cms.double(4.),
 
+	cut1_apply = cms.bool(False),
+	cut1_a = cms.double(1),
+	cut1_c = cms.double(0),
+	cut1_si = cms.double(0),
+
+	cut2_apply = cms.bool(False),
+	cut2_a = cms.double(1),
+	cut2_c = cms.double(0),
+	cut2_si = cms.double(0),
+
+	cut3_apply = cms.bool(False),
+	cut3_a = cms.double(1),
+	cut3_c = cms.double(0),
+	cut3_si = cms.double(0),
+
+	cut4_apply = cms.bool(False),
+	cut4_a = cms.double(1),
+	cut4_c = cms.double(0),
+	cut4_si = cms.double(0)
+)
