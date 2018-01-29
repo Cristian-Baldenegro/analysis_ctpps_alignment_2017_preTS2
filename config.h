c05607e0dfaaf38d0d3f8546f67bd270747737d4
Jan Kašpar <jan.kaspar@cern.ch>
2018-01-29 11:50:50 +0100

    Loads also alignment corrections.

--- config.h
+++ config.h
@@ -27,6 +27,8 @@ struct Config
 {
 	vector<string> input_files;
 
+	map<unsigned int, double> alignment_corrections_x;
+
 	bool aligned;
 
 	double n_si;
@@ -53,12 +55,26 @@ struct Config
 
 int Config::LoadFrom(const string &f_in)
 {
+	map<unsigned int, string> rp_tags = {
+		{ 3, "L_2_F" },
+		{ 2, "L_1_F" },
+		{ 102, "R_1_F" },
+		{ 103, "R_2_F" }
+	};
+
 	const edm::ParameterSet& config = edm::readPSetsFrom(f_in)->getParameter<edm::ParameterSet>("config");
 
 	input_files = config.getParameter<vector<string>>("input_files");
 
+	const auto &acc = config.getParameter<edm::ParameterSet>("alignment_corrections");
+	for (const auto &p : rp_tags)
+	{
+		const auto &ps = acc.getParameter<edm::ParameterSet>("rp_" + p.second);
+		alignment_corrections_x[p.first] = ps.getParameter<double>("de_x");
+	}
+
 	aligned = config.getParameter<bool>("aligned");
-	
+
 	n_si = config.getParameter<double>("n_si");
 
 	cut1_apply = config.getParameter<bool>("cut1_apply");
@@ -81,25 +97,14 @@ int Config::LoadFrom(const string &f_in)
 	cut4_c = config.getParameter<double>("cut4_c");
 	cut4_si = config.getParameter<double>("cut4_si");
 
+	const auto &c_m1d = config.getParameter<edm::ParameterSet>("matching_1d");
+	matching_1d_reference_datasets = c_m1d.getParameter<vector<string>>("reference_datasets");
+
+	for (const auto &p : rp_tags)
 	{
-		const auto &c_m1d = config.getParameter<edm::ParameterSet>("matching_1d");
-		matching_1d_reference_datasets = c_m1d.getParameter<vector<string>>("reference_datasets");
-
-		const auto &c_m1d_L_2_F = c_m1d.getParameter<edm::ParameterSet>("rp_L_2_F");
-		matching_1d_ranges[3] = SelectionRange(c_m1d_L_2_F.getParameter<double>("x_min"), c_m1d_L_2_F.getParameter<double>("x_max"));
-		matching_1d_shift_ranges[3] = SelectionRange(c_m1d_L_2_F.getParameter<double>("sh_min"), c_m1d_L_2_F.getParameter<double>("sh_max"));
-
-		const auto &c_m1d_L_1_F = c_m1d.getParameter<edm::ParameterSet>("rp_L_1_F");
-		matching_1d_ranges[2] = SelectionRange(c_m1d_L_1_F.getParameter<double>("x_min"), c_m1d_L_1_F.getParameter<double>("x_max"));
-		matching_1d_shift_ranges[2] = SelectionRange(c_m1d_L_1_F.getParameter<double>("sh_min"), c_m1d_L_1_F.getParameter<double>("sh_max"));
-
-		const auto &c_m1d_R_1_F = c_m1d.getParameter<edm::ParameterSet>("rp_R_1_F");
-		matching_1d_ranges[102] = SelectionRange(c_m1d_R_1_F.getParameter<double>("x_min"), c_m1d_R_1_F.getParameter<double>("x_max"));
-		matching_1d_shift_ranges[102] = SelectionRange(c_m1d_R_1_F.getParameter<double>("sh_min"), c_m1d_R_1_F.getParameter<double>("sh_max"));
-
-		const auto &c_m1d_R_2_F = c_m1d.getParameter<edm::ParameterSet>("rp_R_2_F");
-		matching_1d_ranges[103] = SelectionRange(c_m1d_R_2_F.getParameter<double>("x_min"), c_m1d_R_2_F.getParameter<double>("x_max"));
-		matching_1d_shift_ranges[103] = SelectionRange(c_m1d_R_2_F.getParameter<double>("sh_min"), c_m1d_R_2_F.getParameter<double>("sh_max"));
+		const auto &ps = c_m1d.getParameter<edm::ParameterSet>("rp_" + p.second);
+		matching_1d_ranges[p.first] = SelectionRange(ps.getParameter<double>("x_min"), ps.getParameter<double>("x_max"));
+		matching_1d_shift_ranges[p.first] = SelectionRange(ps.getParameter<double>("sh_min"), ps.getParameter<double>("sh_max"));
 	}
 
 	return 0;
