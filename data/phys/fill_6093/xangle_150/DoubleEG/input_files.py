20c2f8550b626b239d2513626e2e0c52164ae586
Jan Kašpar <jan.kaspar@cern.ch>
2018-01-31 10:25:46 +0100

    Added configs for all 150urad datasets.

---
+++ data/phys/fill_6093/xangle_150/DoubleEG/input_files.py
@@ -0,0 +1,5 @@
+import FWCore.ParameterSet.Config as cms
+
+input_files = cms.vstring(
+    "root://eostotem.cern.ch//eos/totem/data/ctpps/reconstruction/2017/preTS2_alignment_data/version1/fill6093_xangle150_DoubleEG.root"
+)
